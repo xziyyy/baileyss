958cc802f6e3ee5df2ebde87299606016bc06680
nstar-y <gimnastiarbahri12@sma.belajar.id>
2025-05-16 00:21:36 +0700

    update

--- src/Socket/groups.ts
+++ src/Socket/groups.ts
@@ -344,7 +344,7 @@ export const extractGroupMetadata = (result: BinaryNode) => {
 	const memberAddMode = getBinaryNodeChildString(group, 'member_add_mode') === 'all_member_add'
 	const metadata: GroupMetadata = {
 		id: groupId,
-		addressingMode: group.attrs.addressing_mode,
+		addressingMode: group.attrs.addressing_mode as "pn" | "lid",
 		subject: group.attrs.subject,
 		subjectOwner: group.attrs.s_o,
 		subjectOwnerPhoneNumber: group.attrs.s_o_pn,
